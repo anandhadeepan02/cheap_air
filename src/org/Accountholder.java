b1f8a0fb358ef5681fd300bac75a6568d0404fd9
anandhadeepan02 <anandhadeepan02@gmail.com>
2024-01-27 23:45:09 +0530

    commit

---
+++ src/org/Accountholder.java
@@ -0,0 +1,14 @@
+package org;
+
+public class Accountholder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Bank a= new Bank();
+System.out.println(a.getmin());
+
+a.setmin(100);
+System.out.println(a.getmin());
+	}
+
+}
---
+++ src/org/Bank.java
@@ -0,0 +1,19 @@
+ package org;
+
+public class Bank {
+private int minimumbalance=1000;
+
+public int getmin() {
+	return minimumbalance;
+}
+
+public void setmin(int sum) {
+	if(1000<sum) 
+		this. minimumbalance=sum;
+	
+	
+}
+
+
+
+}
